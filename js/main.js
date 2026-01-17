4cc8395a7b8a8cda187e8d59b41c6e6042d5c7c8
braulio ortiz <ortizcastrobrauliojose6@gmail.com>
2026-01-16 20:26:20 -0600

    feat(feature-branch): logo added to the nav-bar . html adjusted.

--- js/main.js
+++ js/main.js
@@ -58,4 +58,18 @@ mapButtons.forEach(button => {
     }
   });
 });
+
+// ==== HAMBURGER-MENU ====
+    let menuList = document.getElementById("menuList")
+      menuList.style.maxHeight = "0px";
+
+      function toggleMenu() {
+        if(menuList.style.maxHeight == "0px")
+      {
+         menuList.style.maxHeight = "300px";
+      }
+      else{
+        menuList.style.maxHeight = "0px";
+      }
+      }
  
